49a5a8e60c1a915a7b98f74f91c64390b6fdbada
Nicole <en.lemap@uea.edu.ec>
2026-07-05 11:18:50 -0500

    Tarea Semana 6 - Bootstrap y CSS3

--- script.js
+++ script.js
@@ -5,9 +5,101 @@ const total = document.getElementById("total");
 
 let contador = 0;
 
+function validarNombre(){
+
+    const nombre = document.getElementById("nombre");
+
+    const error = document.getElementById("errorNombre");
+
+    if(nombre.value.trim().length < 3){
+
+        nombre.classList.add("is-invalid");
+
+        nombre.classList.remove("is-valid");
+
+        error.textContent = "Debe tener al menos 3 caracteres.";
+
+        return false;
+
+    }
+
+    nombre.classList.remove("is-invalid");
+
+    nombre.classList.add("is-valid");
+
+    error.textContent="";
+
+    return true;
+
+}
+
+function validarDescripcion(){
+
+    const descripcion = document.getElementById("descripcion");
+
+    const error = document.getElementById("errorDescripcion");
+
+    if(descripcion.value.trim().length < 10){
+
+        descripcion.classList.add("is-invalid");
+
+        descripcion.classList.remove("is-valid");
+
+        error.textContent="La descripción debe tener al menos 10 caracteres.";
+
+        return false;
+
+    }
+
+    descripcion.classList.remove("is-invalid");
+
+    descripcion.classList.add("is-valid");
+
+    error.textContent="";
+
+    return true;
+
+}
+
+function validarCategoria(){
+
+    const categoria=document.getElementById("categoria");
+
+    const error=document.getElementById("errorCategoria");
+
+    if(categoria.value===""){
+
+        categoria.classList.add("is-invalid");
+
+        categoria.classList.remove("is-valid");
+
+        error.textContent="Seleccione una categoría.";
+
+        return false;
+
+    }
+
+    categoria.classList.remove("is-invalid");
+
+    categoria.classList.add("is-valid");
+
+    error.textContent="";
+
+    return true;
+
+}
+
 formulario.addEventListener("submit", function(e) {
 
     e.preventDefault();
+
+    if(
+    !validarNombre() ||
+    !validarDescripcion() ||
+    !validarCategoria()
+){
+    return;
+}
 
     const nombre = document.getElementById("nombre").value;
     const descripcion = document.getElementById("descripcion").value;
@@ -57,5 +149,11 @@ formulario.addEventListener("submit", function(e) {
     });
 
     formulario.reset();
+
+ document.getElementById("nombre").addEventListener("input", validarNombre);
+
+document.getElementById("descripcion").addEventListener("input", validarDescripcion);
+
+document.getElementById("categoria").addEventListener("change", validarCategoria);
 
 });
